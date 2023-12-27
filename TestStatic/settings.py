b888c46a93f5634b628606f3ecb475b3556218f2
L1vDev <livdev.rguez@gmail.com>
2023-12-27 16:26:12 -0500

    Fix Database

--- TestStatic/settings.py
+++ TestStatic/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'TestStatic.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
